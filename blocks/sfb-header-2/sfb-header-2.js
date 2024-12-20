62af71d7d895d38e917f4d715b57b75b9b702b56
eds-tcz <edstechchefz@gmail.com>
2024-12-20 17:01:00 +0530

    fix

--- blocks/sfb-header-2/sfb-header-2.js
+++ blocks/sfb-header-2/sfb-header-2.js
@@ -1,24 +1,24 @@
 export default function decorate(block) {
   [...block.children].forEach((row) => {
-    row.classList.add("header-cont");
+    row.classList.add('header-cont');
     [...row.children].forEach((div, d) => {
       if (d === 0) {
-        div.classList.add("header-logo");
+        div.classList.add('header-logo');
       }
       if (d === 1) {
-        div.classList.add("main-nav");
-        const ultag = div.querySelector("ul");
-        ultag.classList.add("header-nav-list");
+        div.classList.add('main-nav');
+        const ultag = div.querySelector('ul');
+        ultag.classList.add('header-nav-list');
       }
       if (d === 2) {
-        div.classList.add("action-container", "d-flex", "align-items-center");
-        const ptag = div.querySelectorAll("p");
+        div.classList.add('action-container', 'd-flex', 'align-items-center');
+        const ptag = div.querySelectorAll('p');
         ptag.forEach((p, i) => {
           if (i === 0) {
-            p.classList.add("btn-login");
+            p.classList.add('btn-login');
           }
           if (i === 1) {
-            p.classList.add("search-icon");
+            p.classList.add('search-icon');
           }
         });
       }
